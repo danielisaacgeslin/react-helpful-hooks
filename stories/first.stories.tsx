722d6db6871bc083edc7b3193f67072ba448aa26
Daniel Isaac Geslin <isaacgeslin@gmail.com>
2020-04-20 14:49:28 +0200

    adding deps

---
+++ stories/first.stories.tsx
@@ -0,0 +1,6 @@
+import * as React from "react";
+import { storiesOf } from "@storybook/react";
+
+storiesOf("Button", module)
+  .add("with text", () => <button>Hello button</button>)
+  .add("with some emoji", () => <button>😀 😎 👍 💯</button>);
