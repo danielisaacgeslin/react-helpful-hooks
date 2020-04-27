681588cdac55c491f95b126fd626103966d21314
Daniel Isaac Geslin <isaacgeslin@gmail.com>
2020-04-27 14:10:21 +0200

    forgot to memorize useStateHistory setState ref

--- src/useStateHistory.ts
+++ src/useStateHistory.ts
@@ -1,8 +1,10 @@
-import { useState, Dispatch, SetStateAction } from 'react';
+import { useState, Dispatch, SetStateAction, useCallback } from 'react';
 
 export const useStateHistory = <T>(initialValue?: T, limit: number = Infinity): [T, Dispatch<SetStateAction<T>>, T[]] => {
   const [history, setHistory] = useState<T[]>([initialValue].filter(v => v !== undefined) as T[]);
-  const setState = (s: T | ((p: T) => T)) =>
-    setHistory(prevState => [typeof s === 'function' ? (s as Function)(prevState[0]) : s, ...prevState].slice(0, limit));
+  const setState = useCallback(
+    (s: T | ((p: T) => T)) => setHistory(prevState => [typeof s === 'function' ? (s as Function)(prevState[0]) : s, ...prevState].slice(0, limit)),
+    [setHistory]
+  );
   return [history[0], setState, history];
 };
